417b4916ee3a4223ab1be387e5ea4bbdbf1357bd
Ryan Kelley <20136584@tafe.wa.edu.au>
2025-07-22 12:33:52 +0800

    chore: Start of project

---
+++ app/Models/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    public function jokes(): BelongsToMany
+    {
+        return $this->belongsToMany(Joke::class);
+    }
+
+    public function jokesByTitleDesc(): BelongsToMany
+    {
+        return $this->jokes()->orderBy('title', 'desc');
+    }
+
+    public function jokesByDateAddedDesc(): BelongsToMany
+    {
+        return $this->jokes()->orderBy('created_at', 'desc');
+    }
+}
---
+++ database/seeders/JokeSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Joke;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class JokeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $seedJokes = [
+            [
+                'title' => 'Skeleton Fight',
+                'content' => "What don't skeletons fight eachother? ".
+                            "Because they don't have the guts.",
+                'user_id' => 100,
+                'categories' => ['Skeleton'],
+            ],
+            [
+                'title' => 'Pirate Maths',
+                'content' => 'What type of maths are pirates best at?'.
+                    'Algebra. because they are good at finding X',
+                'user_id' => 100,
+                'categories' => ['Pirate', 'Maths'],
+            ],
+        ];
+
+        $users = User::all()->pluck('id', 'id')->toArray();
+        foreach ($seedJokes as $seedJoke) {
+            $categoryList = $seedJoke['categories'] ?? 'Unknown';
+            unset($seedJoke['categories']);
+
+            $joke = Joke::updateOrCreate([
+                'title' => $seedJoke['title'],
+                'content' => $seedJoke['content'],
+                'user_id' => $users[array_rand($users)],
+            ]);
+
+            foreach ($categoryList as $category) {
+                Category::updateOrCreate(['title' => $category]);
+            }
+            if (! empty($categoryList)) {
+                $categoryIds = Category::whereIn('title', $categoryList)
+                    ->get()
+                    ->pluck('id')
+                    ->toArray();
+                $joke->categories()->sync($categoryIds);
+            }
+        }
+    }
+}
